e03ae52ee7d116b9ea1375f6c3f2bc08163450f9
“mahfuz-7148” <mrahman7148@gmail.com>
2024-12-19 00:22:15 +0600

    'up'

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react'
-import UserData from './pages/UserData'
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // useEffect(() => {
-  //   window.alert('alertinggg')
-  // },[])
-
-  // useEffect(() => {
-  //   window.alert('changedd')
-  // },[count])
-
-  const [allData, setAllData] = useState([])
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setAllData(data))
+    window.alert('alertinggg')
   },[])
 
-  return (
-    // <div>
-    //   <h1>Count {count} </h1>
-    //   <button onClick={()=> setCount(count + 1) } className='bg-green-600 text-white p-2'>Click me</button>
-    // </div>
+  useEffect(() => {
+    window.alert('changedd')
+  },[count])
 
+ 
+
+  return (
     <div>
-      {/* <h1 className='text-3xl'>Users</h1>
-      <h1 className='text-xl'>Data length: {allData.length} </h1> */}
-     { allData.map(data => 
-    //  <h1>{data.name} </h1>
-       <UserData 
-       key={data.id}
-       data = {data}
-       />
-    )}
+      <h1>Count {count} </h1>
+      <button onClick={()=> setCount(count + 1) } className='bg-green-600 text-white p-2'>Click me</button>
     </div>
   )
 }
